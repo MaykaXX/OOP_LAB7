81127a1c610f65362d163b5a0cbaf9957b17dc8a
Maria Vodko <163645840+MaykaXX@users.noreply.github.com>
2026-04-09 12:49:50 +0300

    add menu for print work of lists

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "../termcolor/termcolor.hpp"
 #include "SingleList.cpp"
 
-int main() {
+void Double_list(){};
 
-SingleList<int> single_list(5);
+void Single_list () {
+    SingleList<int> single_list(5);
     single_list.push_back(10);
     single_list.push_front(20);
     single_list.push_front(30);
@@ -28,7 +30,25 @@ SingleList<int> single_list(5);
     single_list.printList();
 
     std::cout << "index: "<< single_list.find(333) <<std::endl;
+}
 
+int main() {
+    int choice = 0;
+    std::cout<< termcolor::bright_magenta<<"~~~~Work with Lists~~~~"<<termcolor::reset<<std::endl;
+    std::cout << "1. Single list" << std::endl;
+    std::cout << "2. Double list" << std::endl;
+    std::cout << "3. Exit" << std::endl;
+
+    while (choice != 3) {
+        std::cout << "Choice: ";
+        std::cin >> choice;
+        switch (choice) {
+            case 1:
+                Single_list();
+            case 2:
+                Double_list();
+        }
+    }
     return 0;
 
 }
